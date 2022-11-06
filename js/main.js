6d7074c5050dd12a6d95c07df7419c54d67f9009
cabbagecabbagecabbage <48808428+cabbagecabbagecabbage@users.noreply.github.com>
2022-11-06 10:19:55 -0500

    Improved nav burger

--- js/main.js
+++ js/main.js
@@ -7,30 +7,11 @@ const toggleNavLinks = () => {
   navLinksNode.classList.toggle("nav-links-active");
   navBurgerNode.classList.toggle("nav-burger-active");
   navNode.classList.toggle("nav-active");
+  if (navBurgerNode.classList.contains("nav-burger-active")){
+    navBurgerNode.style.borderBottomRightRadius = 0;
+    navBurgerNode.style.borderBottomLeftRadius = 0;
+  }
+  else {
+    navBurgerNode.style.borderRadius = "1rem";
+  }
 };
-
-// //timer stuff
-// var countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function () {
-//   // Get today's date and time
-//   var now = new Date().getTime();
-
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//   document.getElementById("timer").innerHTML =
-//     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("timer").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
